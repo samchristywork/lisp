5937f0de74c4d65a0d2c9b54eaa49dcc98095ac5
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-10-05 14:50:06 -0500

    feat: Add functions to `env`
    These functions bind new symbols, mark as symbol as undefined, and
    evaluate some math functions.

--- src/env.c
+++ src/env.c
@@ -1,3 +1,5 @@
+#include <math.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "node.h"
@@ -17,6 +19,27 @@ void add_float_binding(struct environment **env, char *symbol, float value) {
   var->type = FLOAT;
   var->floating = value;
   add_binding(env, symbol, var);
+}
+
+void add_function_binding(
+    struct environment **env, char *symbol,
+    struct expression *(*function)(struct expression *, struct environment *)) {
+  struct expression *var = malloc(sizeof(struct expression));
+  var->type = FUNCTION;
+  var->function = function;
+  add_binding(env, symbol, var);
+}
+
+struct expression *eval_sin(struct expression *arg, struct environment *env) {
+  struct expression *result = malloc(sizeof(struct expression));
+  result->type = FLOAT;
+  result->floating = sin(arg->children[1].floating);
+  return result;
+}
+
+struct expression *undefined(struct expression *arg, struct environment *env) {
+  printf("Undefined function\n");
+  return NULL;
 }
 
 struct environment *standard_env() {
