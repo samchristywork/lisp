693f14132ee753d4c970730756c1899e4899cb36
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-10-05 14:51:34 -0500

    fix: Be sure to declare `add_function_binding` in header
    I had defined this function in the source file without adding it to the
    corresponding header file.

--- src/env.h
+++ src/env.h
@@ -8,6 +8,10 @@ void add_binding(struct environment **env, char *symbol,
 
 void add_float_binding(struct environment **env, char *symbol, float value);
 
+void add_function_binding(struct environment **env, char *symbol,
+                          struct expression *(*function)(struct expression *,
+                                                         struct environment *));
+
 struct environment *standard_env();
 
 #endif
